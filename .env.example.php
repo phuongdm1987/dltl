02978ebef0af6acce1c6b38fe74eb6d999bd3a12
Henry - phuongdm <phuongdm@coccoc.com>
2020-12-02 14:30:48 +0700

    add gitignore file and env.example file

---
+++ .env.example.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+return [
+    'APP_ENV' => 'local',
+    'APP_KEY' => 'cnMH5sXR5RweeXoXAkvLB5tEysavtLPL',
+    'DB_HOST' => 'mysql',
+    'DB_USER' => 'henry',
+    'DB_PASS' => 'secret',
+    'DB_NAME' => 'dltl',
+
+    'FACEBOOK_CLIENT_ID' => '',
+    'FACEBOOK_SECRETKEY' => '',
+
+    'GOOGLE_CLIENT_ID' => '',
+    'GOOGLE_SECRETKEY' => '',
+];
