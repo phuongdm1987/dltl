e056d85fe42ca461b1a7ecc36c3d453a2f523609
Henry - phuongdm <phuongdm@coccoc.com>
2022-04-25 17:19:51 +0700

    remove tawk chat box

--- app/views/dltl/layouts/master.blade.php
+++ app/views/dltl/layouts/master.blade.php
@@ -49,19 +49,6 @@
         "imageSize": 24
     });
 </script>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/624cf49bc72df874911d2a51/1fvu9n0rv';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
 
 <!-- Messenger Plugin chat Code -->
 <div id="fb-root"></div>
