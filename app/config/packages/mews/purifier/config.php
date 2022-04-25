f617bd2a47d1bf60579345506435ab6e29b9d66c
Henry - phuongdm <phuongdm@coccoc.com>
2022-04-25 17:14:32 +0700

    allow heading tag

--- app/config/packages/mews/purifier/config.php
+++ app/config/packages/mews/purifier/config.php
@@ -15,7 +15,7 @@ return array(
     'settings' => array(
         'default' => array(
             'HTML.Doctype'             => 'XHTML 1.0 Strict',
-            'HTML.Allowed'             => 'div,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+            'HTML.Allowed'             => 'div,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],h1,h2,h3,h4,h5,h6',
             'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
             'AutoFormat.AutoParagraph' => false,
             'AutoFormat.RemoveEmpty'   => true,
